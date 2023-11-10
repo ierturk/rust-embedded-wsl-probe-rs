d656e5abeebd5a70eb67c05c129ea24ebd1f942d
Ibrahim Erturk <me.erturk.me>
2023-11-10 22:26:45 +0300

    Display is functional

---
+++ src/display.rs
@@ -0,0 +1,95 @@
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    peripherals::{self},
+    spi,
+};
+use embedded_graphics::{
+    mono_font::MonoTextStyle,
+    pixelcolor::Rgb565,
+    prelude::RgbColor,
+    primitives::{PrimitiveStyle, PrimitiveStyleBuilder},
+    text::{Baseline, TextStyle},
+};
+use {defmt_rtt as _, panic_probe as _};
+
+use display_interface_spi::SPIInterface;
+use embassy_stm32::dma::NoDma;
+use embassy_stm32::time::Hertz;
+use ili9341::{Ili9341, Orientation};
+
+pub type DisplayInterface = Ili9341<
+    SPIInterface<
+        embassy_stm32::spi::Spi<'static, peripherals::SPI5, NoDma, NoDma>,
+        Output<'static, peripherals::PD13>,
+        Output<'static, peripherals::PC2>,
+    >,
+    Output<'static, peripherals::PG14>,
+>;
+
+pub struct Display {
+    pub interface: DisplayInterface,
+    pub styles: Styles,
+}
+
+pub fn init(
+    mosi: peripherals::PF9,
+    clk: peripherals::PF7,
+    cs: peripherals::PC2,
+    reset: peripherals::PG14,
+    dc: peripherals::PD13,
+    spi: peripherals::SPI5,
+) -> Display {
+    let cs = Output::new(cs, Level::High, Speed::VeryHigh);
+    let reset = Output::new(reset, Level::Low, Speed::VeryHigh);
+    let dc = Output::new(dc, Level::Low, Speed::VeryHigh);
+
+    let display_spi = {
+        let mut config = spi::Config::default();
+        config.frequency = Hertz(16_000_000);
+        config.mode = ili9341::SPI_MODE;
+        spi::Spi::new_txonly(spi, clk, mosi, NoDma, NoDma, config)
+    };
+
+    let interface: DisplayInterface = {
+        let mut delay = embassy_time::Delay {};
+        Ili9341::new(
+            SPIInterface::new(display_spi, dc, cs),
+            reset,
+            &mut delay,
+            Orientation::LandscapeFlipped,
+            ili9341::DisplaySize240x320,
+        )
+        .unwrap()
+    };
+
+    let styles = Styles::new();
+
+    Display { interface, styles }
+}
+
+/// Some shared styles
+pub struct Styles {
+    pub char: MonoTextStyle<'static, Rgb565>,
+    pub text: TextStyle,
+    pub black_fill: PrimitiveStyle<Rgb565>,
+    pub white_fill: PrimitiveStyle<Rgb565>,
+}
+
+impl Styles {
+    fn new() -> Styles {
+        let char = MonoTextStyle::new(&profont::PROFONT_24_POINT, Rgb565::WHITE);
+        let text = TextStyle::with_baseline(Baseline::Top);
+        let black_fill = PrimitiveStyleBuilder::new()
+            .fill_color(Rgb565::BLACK)
+            .build();
+        let white_fill = PrimitiveStyleBuilder::new()
+            .fill_color(Rgb565::WHITE)
+            .build();
+        Styles {
+            char,
+            text,
+            black_fill,
+            white_fill,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,143 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::cell::RefCell;
 use defmt::*;
+use display::Display;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::Timer;
+use embassy_stm32::{gpio, peripherals};
+use embassy_sync::blocking_mutex::raw::{CriticalSectionRawMutex, ThreadModeRawMutex};
+use embassy_sync::blocking_mutex::Mutex;
+use embassy_sync::signal::Signal;
+use embassy_time::{Duration, Timer};
+use gpio::{Input, Level, Output, Pull, Speed};
 use {defmt_rtt as _, panic_probe as _};
 
+use embedded_graphics::{
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyleBuilder, Rectangle},
+    text::Text,
+};
+
+mod display;
+
+type LedOutput = Output<'static, peripherals::PG13>;
+type ButtonInput = Input<'static, peripherals::PA0>;
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
+    info!("Program start");
+
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    let led: LedOutput = Output::new(p.PG13, Level::Low, Speed::Low);
+    let button: ButtonInput = Input::new(p.PA0, Pull::Up);
 
-    let mut led_green = Output::new(p.PG13, Level::High, Speed::Low);
-    let mut led_red = Output::new(p.PG14, Level::High, Speed::Low);
+    let display = display::init(p.PF9, p.PF7, p.PC2, p.PG14, p.PD13, p.SPI5);
 
+    unwrap!(spawner.spawn(blinker(led, Duration::from_millis(500))));
+    // unwrap!(spawner.spawn(button_monitor(button)));
+    unwrap!(spawner.spawn(display_refresh(display)));
+}
+
+/// Blink the physical LED, and a matching indicator on the LCD display
+#[embassy_executor::task]
+async fn blinker(mut led: LedOutput, interval: Duration) {
+    let mut blink = false;
     loop {
-        info!("high");
-        led_green.set_high();
-        led_red.set_low();
-        Timer::after_millis(1000).await;
+        led.set_level(if blink { Level::Low } else { Level::High });
+        display_state_update(|mut s| s.indicator1 = blink);
+        blink = !blink;
+        Timer::after(interval).await;
+    }
+}
 
-        info!("low");
-        led_green.set_low();
-        led_red.set_high();
-        Timer::after_millis(1000).await;
+/// Monitor the button, and show an indicator on the LCD display
+#[embassy_executor::task]
+async fn button_monitor(mut button: ButtonInput) {
+    loop {
+        // button.wait_for_any_edge().await;
+        let level = button.get_level();
+        display_state_update(|mut s| s.indicator2 = level == Level::High);
+    }
+}
+
+#[derive(Clone)]
+struct DisplayState {
+    indicator1: bool,
+    indicator2: bool,
+}
+
+static DISPLAY_STATE: Mutex<ThreadModeRawMutex, RefCell<DisplayState>> =
+    Mutex::new(RefCell::new(DisplayState {
+        indicator1: false,
+        indicator2: false,
+    }));
+static DISPLAY_SIGNAL: Signal<CriticalSectionRawMutex, ()> = Signal::new();
+
+// Keep the display up to date
+#[embassy_executor::task]
+async fn display_refresh(mut display: Display) {
+    render_background(&mut display);
+    loop {
+        DISPLAY_SIGNAL.wait().await;
+        let state = DISPLAY_STATE.lock(|s| s.borrow().clone());
+        render_indicator(&mut display, Point::new(120, 120), state.indicator1);
+        render_indicator(&mut display, Point::new(180, 120), state.indicator2);
+    }
+}
+
+fn display_state_update<F>(mut sfn: F)
+where
+    F: FnMut(&mut DisplayState) -> (),
+{
+    DISPLAY_STATE.lock(|s| sfn(&mut s.borrow_mut()));
+    DISPLAY_SIGNAL.signal(());
+}
+
+fn render_background(display: &mut Display) {
+    let test_text = "Pixel Blinky";
+
+    Rectangle::new(Point::new(0, 0), Size::new(320, 240))
+        .into_styled(display.styles.black_fill)
+        .draw(&mut display.interface)
+        .unwrap();
+
+    Text::with_text_style(
+        test_text,
+        Point::new(60, 0),
+        display.styles.char,
+        display.styles.text,
+    )
+    .draw(&mut display.interface)
+    .unwrap();
+}
+
+/// Draw an "LED" on the LCD display
+fn render_indicator(display: &mut Display, centre: Point, state: bool) -> () {
+    let led_size: u32 = 30;
+    let led_at = Point::new(
+        centre.x - (led_size as i32) / 2,
+        centre.y - (led_size as i32) / 2,
+    );
+
+    if state {
+        Circle::new(led_at, led_size)
+            .into_styled(
+                PrimitiveStyleBuilder::new()
+                    .fill_color(Rgb565::GREEN)
+                    .build(),
+            )
+            .draw(&mut display.interface)
+            .unwrap();
+    } else {
+        Circle::new(led_at, led_size)
+            .into_styled(
+                PrimitiveStyleBuilder::new()
+                    .fill_color(Rgb565::CSS_DARK_GRAY)
+                    .build(),
+            )
+            .draw(&mut display.interface)
+            .unwrap();
     }
 }
